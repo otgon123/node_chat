267b3e46d9738f3d5b17f1ddd00440ed87c7c0c9
otgon123 <220054eo@yse-c.net>
2023-12-13 11:39:59 +0900

    update

---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+const URL = "";
+const socket = io.connect(URL);
+
+
+function sendMessage() {
+    console.log('send message')
+    var message = document.getElementById('message').value;
+    console.log(message);
+    if (message) {
+        var data = {
+            message: message
+        }
+        socket.emit('chat_message', data);
+    }
+}
+
